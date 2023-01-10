c968867bf673de549c7f72336b7bfa51586bb8c6
Erika Faustino <dossantos.erika08@gmail.com>
2023-01-10 16:31:10 -0300

    Create file 06-cli

---
+++ 06-cli/test.js
@@ -0,0 +1,26 @@
+const{
+    deepEqual,
+    ok
+} = require('assert')
+const exp = require('constants')
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: '1'
+}
+
+
+describe('Suite de manipulação de Herois', () =>{
+ it('deve pesquisar um heroi usando arquivis', async ()=> {
+    const expected = DEFAULT_ITEM_CADASTRAR
+
+    ok(null, expected)
+ })
+
+    //it('deve cadastrar um heroi, usando arquivos', async() => {
+        //const expected = DEFAULT_ITEM_CADASTRAR
+        //
+        //ok(null, expected)
+   // })
+})
