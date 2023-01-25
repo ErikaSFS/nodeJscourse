acfca404a9041fd07f391bda6655761e412b90da
Erika Faustino <dossantos.erika08@gmail.com>
2023-01-25 18:52:45 -0300

    Create 06-cli file

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,71 +1,54 @@
 const {
-    readFile, writeFile
+    readFile,
+    writeFile
 } = require('fs')
 
-
-const {
-    promisify
+const {promisify
 } = require('util')
 
-const readFileAsync = promisify(readFile)
-const writeFileAsync = promisify(writeFile)
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+
 
 class Database {
-    constructor(){
+    constructor() {
         this.NOME_ARQUIVO = 'herois.json'
     }
-
     async obterDadosArquivo() {
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
     }
-    async escreverArquivo() {
+    async escreverArquivo(dados) {
         await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
-        return true
+        return true;
+    }
 
-        const heroidcomId = {
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+
+
+
+        const heroiComId = {
+            ...heroi,
             id,
-            ...heroi
         }
 
         const dadosFinal = [
             ...dados,
-            heroidcomId
+            heroiComId
         ]
-
         const resultado = await this.escreverArquivo(dadosFinal)
         return resultado;
-    }
 
-    async cadastar(heroi){
-        const dados = await this.obterDadosArquivo()
-        const id = heroi.id <= 2 ? heroi.id : Date.now();
+
 
     }
-    async listar(id){
+     async listar(id){
         const dados = await this.obterDadosArquivo()
-        const dadosFiltrados = dados.filter(item => (id ? (item.id == id) :true))
+        const dadosFiltrados = dados.filter( item => (id ? (item.id === id) : true))
         return dadosFiltrados
-    
     }
-
-    async remover(id) {
-        if(!id) {
-            return await this.escreverArquivo([])
-        }
-
-        const dados = await this.obterDadosArquivo()
-        const indice = dados.findIndice( item => item.id === parseInt(id));
-        if (!indice === -1) {
-            throw Error('o usuario não existe');
-        } 
-        dados.splice(indice, 1);
-
-        return await this.escreverArquivo(dados);
-
-
-    }
-    
 }
 
-module.exports = new Database();
+module.exports = new Database()
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,39 +1,36 @@
-const{
+const {
     deepEqual,
     ok
-} = require('assert')
+
+} = require ('assert')
 
 const database = require('./database')
 
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
-    poder: 'Speed',
+    poder: 'Speed', 
     id: '1'
+
 }
 
-
-describe('Suite de manipulação de Herois', () => {
+describe ('Suite de manipulacao de Herois', () => {
     before(async () => {
-        await database.cadastar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
     })
     
-    it('deve pesquisar um heroi, usando arquivos', async ()=> {
-    const expected = DEFAULT_ITEM_CADASTRAR
-    const [resultado] = await database.listar(expected.id)
-
-    deepEqual(resultado, expected)
- })
-    it('deve cadastrar um heroi, usando arquivos', async() => {
+    it('Deve pesquisar um heroi usando arquivos', async() => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        const resultado = await database.cadastar(DEFAULT_ITEM_CADASTRAR)
+        const [resultado] = await database.listar()
+        
+        deepEqual(resultado, expected);
+    })
+
+
+   it('Deve cadastrar um heroi, usando arquivos', async() => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
         
-        deepEqual(actual, expected)
-   })
-
-   it('deve remover um heroi por id', async ()=> {
-    const expected = true;
-    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
-    deepEqual(resultado, expected)
+        deepEqual(actual, expected);
     })
 })
